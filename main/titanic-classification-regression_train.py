5fd65e06aecb8148f5fdd166a181ccc4e82dedeb
Bhargava Sure <bhargava.s@valuelabs.com>
2021-03-08 11:56:36 +0530

    code chnge

--- main/titanic-classification-regression_train.py
+++ main/titanic-classification-regression_train.py
@@ -8,7 +8,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt;
 import seaborn as sns
-get_ipython().run_line_magic('matplotlib', 'inline')
+# get_ipython().run_line_magic('matplotlib', 'inline')
 
 
 # In[2]:
