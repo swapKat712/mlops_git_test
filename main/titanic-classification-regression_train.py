a11ae2f3858d87f90549f3b9e7eab0be3f8e36f9
swapKat <47251006+swapKat712@users.noreply.github.com>
2022-09-02 19:16:57 +0530

    Update titanic-classification-regression_train.py

--- main/titanic-classification-regression_train.py
+++ main/titanic-classification-regression_train.py
@@ -6,7 +6,7 @@
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt;
+#import matplotlib.pyplot as plt;
 import seaborn as sns
 # get_ipython().run_line_magic('matplotlib', 'inline')
 
