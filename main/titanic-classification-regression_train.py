fcc1a7d7f036a1287b0265bdaf1129560c60d72b
swapKat <47251006+swapKat712@users.noreply.github.com>
2022-11-15 13:27:46 +0530

    Update titanic-classification-regression_train.py

--- main/titanic-classification-regression_train.py
+++ main/titanic-classification-regression_train.py
@@ -15,7 +15,7 @@ import numpy as np
 
 print("reading dataset")
 try:
-    train = pd.read_csv('./train_mod2.csv')
+    train = pd.read_csv('./train_mod.csv')
 except Exception as e:
     print(str(e))
     print(repr(e))
